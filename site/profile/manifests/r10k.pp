5b177fb3cad4d45dfdfd0ac3efdf637597fefc7e
terrazasrjAEXP <145479154+terrazasrjAEXP@users.noreply.github.com>
2023-12-14 11:44:21 -0500

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,6 +1,6 @@
 class profile::r10k {
   class {'r10k':
-    remote => 'https://github.com/joshsamuelson/control-repo',
+    remote => 'https://github.com/terrazasrjAEXP/control-repo',
   }
   class {'r10k::webhook::config':
     use_mcollective => false,
